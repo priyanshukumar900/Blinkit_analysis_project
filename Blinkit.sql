6c6ff023bb4eb574ab639b6a6b80d6598bb524b5
Priyanshu Kumar <varunsvar1@gmail.com>
2025-08-05 10:39:59 +0530

    Add files via upload

---
+++ Blinkit.sql
@@ -0,0 +1,94 @@
+use blinkit;
+ 
+ #1 little bit data cleaning
+ set sql_safe_updates=0;
+ update grocery
+ set Item_Fat_Content = 
+ case
+ when Item_Fat_Content = 'LF' then 'Low Fat'
+ when Item_Fat_Content = 'low fat' then 'Low Fat'
+ when Item_Fat_Content = 'reg' then 'Regular'
+ when Item_Fat_Content = 'regular' then 'Regular'
+ else Item_Fat_Content
+ end;
+ 
+#2 Total sales 
+ create view Total_Sale as
+ select round(sum(sales),2) as total_sales from grocery;
+ 
+select * from Total_Sale;
+ 
+#3 Avg sales between year 2015 and 2020
+create view Avg_Sales as
+select round(avg(sales),2)as sales from grocery
+where Outlet_Establishment_Year between 2015 and 2020;
+ 
+select * from Avg_Sales;
+ 
+#4 Total count of different items sold
+create view no_of_items as
+select item_type as item,count(item_type) as No_of_item from grocery
+group by item_type;
+select * from no_of_items;
+
+#5 Average customer rating for each item type
+
+create view Item_Ratings as
+select Item_Type,round(avg(Rating),2) as total_rating from grocery
+group by item_type
+order by total_rating ;
+
+select * from Item_Ratings;
+
+#6 Sales summary according to fields
+
+create view sales_summary as
+select Item_Fat_Content,
+count(Item_Type) as no_of_items,
+round(sum(sales),2) as total_sales,
+round(avg(sales),2) as avg_sales,
+round(avg(rating),2)as Avg_Rating
+from grocery
+group by item_fat_content
+order by Avg_Rating;
+
+select * from sales_summary;
+
+#7 Total Sales By Items
+
+create view sales_by_items as
+SELECT Item_Type, round(SUM(Sales),2) AS Total_Sales from grocery
+GROUP BY Item_Type
+ORDER BY Total_Sales DESC;
+
+select * from sales_by_items;
+
+#8 Total Sales by Outlet Establishment
+
+SELECT Outlet_Establishment_Year, round(SUM(Sales),2) AS Total_Sales
+FROM grocery
+GROUP BY Outlet_Establishment_Year
+ORDER BY Outlet_Establishment_Year; 
+
+#9 creating a short metrics for tables
+
+create view Total_Matrics_by_outlet_type as
+select outlet_type,
+count(item_type)as no_of_items,
+round(sum(sales),2)as total_sales,
+round(avg(sales),2)as avg_sales,
+round(avg(rating),2)as avg_rating 
+from grocery
+
+group by outlet_type
+order by avg_rating;
+
+select * from Total_Matrics_by_outlet_type;
+
+#10 Sales and Avg sales by Outlet Location
+select Outlet_Location_Type, round(sum(Sales),2)as sale, 
+round(avg(Sales),2)as avg_sale from grocery
+
+group by Outlet_Location_Type
+order by sale desc;
+
